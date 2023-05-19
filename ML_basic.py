113b71399206332238d31a8011e7c3699014ca45
VictoriaLiu188 <vliu@research.baycrest.org>
2023-05-19 14:57:48 -0400

    ML basics

---
+++ ML_basic.py
@@ -0,0 +1,10 @@
+# This file is for me to figure out which type of machine learing method to use for the project 
+
+# Supervised: classification and regression 
+
+# https://github.com/school-brainhack/school-brainhack.github.io/blob/main/content/en/modules/machine_learning_basics/machine_learning.ipynb
+
+
+
+
+
